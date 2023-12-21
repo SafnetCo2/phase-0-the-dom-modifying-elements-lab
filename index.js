3d6fba395bae0325c7510e66a05ef8bdf65fc36f
nzi19001@byui.edu <nzi19001@byui.edu>
2023-12-21 16:42:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+//remove main
+main.remove()
+//2) has a 'newHeader' variable that points to node 'h1#victory'
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = '<h1>Josephine is the champion</h1>'
